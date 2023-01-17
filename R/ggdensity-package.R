69128b8c82b4d277ff539693eee86e8513025d1a
jamesotto852 <jamesotto852@gmail.com>
2023-01-17 18:37:22 +0000

    Add useful links to `?ggdensity`

--- R/ggdensity-package.R
+++ R/ggdensity-package.R
@@ -3,6 +3,12 @@
 #' A package that allows more flexible computations for visualization of density
 #' estimates with ggplot2.
 #'
+#' @seealso
+#'
+#' Useful links:
+#' * \url{https://jamesotto852.github.io/ggdensity}
+#' * \url{https://github.com/jamesotto852/ggdensity}
+#'
 #' @import ggplot2
 #' @importFrom MASS bandwidth.nrd kde2d
 #' @importFrom stats uniroot cov pchisq setNames sd cor dnorm
